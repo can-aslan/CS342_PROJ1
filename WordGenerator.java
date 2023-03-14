f9577eec6210a1dee8e252eb2358a8669a631e4a
Yağız Can Aslan <yagizcanaslan0@gmail.com>
2023-03-14 10:50:10 +0300

    first commit

---
+++ WordGenerator.java
@@ -0,0 +1,73 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
+
+public class WordGenerator {
+    final static boolean IS_WORDLIST_MODE = true; // true = take inputs from word list, false = generate random words
+    final static int WORDLIST_LINE_LIMIT = 100;
+    final static String WORDLIST_FILENAME = "wordlist.txt";
+    final static String FILE_FORMAT = "in";
+    final static int NUM_OF_FILES = 4;
+    final static int CHAR_LIMIT = 10; // 63;
+    final static char[] ALPHANUMERIC ="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789#!".toCharArray();
+    final static int MAX_WORDS = 10000;
+
+    public static void main(String[] args) throws IOException {
+        // Generate files
+        List<String> lines = new ArrayList<>();
+        try (BufferedReader br = new BufferedReader(new FileReader(WORDLIST_FILENAME))) {
+            String line;
+            int curLine = 0;
+            while ((line = br.readLine()) != null) {
+                String[] strings = line.split("\n");
+                for (String string : strings) {
+                    lines.add(string);
+                }
+
+                curLine++;
+                if (curLine >= WORDLIST_LINE_LIMIT) {
+                    break;
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        String[] listOfWords = lines.toArray(new String[0]);
+
+        for (int curFile = 0; curFile < NUM_OF_FILES; curFile++) {
+            PrintWriter write = new PrintWriter(new File(FILE_FORMAT + curFile + ".txt"));
+            int words = (int) (Math.random() * MAX_WORDS);
+
+            if (IS_WORDLIST_MODE) {
+                for (int curWord = 0; curWord < words; curWord++) {
+                    write.println(listOfWords[(int) (Math.random() * listOfWords.length)]);
+                }
+            }
+            else { // RANDOM WORD MODE
+                for (int curWord = 0; curWord < words; curWord++) {
+                    write.println(generateWord());
+                }
+            }
+
+            write.close();
+        }
+    }
+
+    public static String generateWord() {
+        // Generate character count of random word
+        int charCount = (int) ((Math.random() * CHAR_LIMIT) + 1);
+        String result = "";
+
+        // Generate the word, character by character
+        for (int i = 0; i < charCount; i++) {
+            result += ALPHANUMERIC[(int) (Math.random() * ALPHANUMERIC.length)];
+        }
+
+        return result;
+    }
+}
